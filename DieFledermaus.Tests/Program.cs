d20b94da4b608ad35fb740b66906e4ce3f1bc4bc
KimikoMuffin <kimiko.muffin@gmail.com>
2015-11-11 18:32:21 -0500

    Updated the test-program so that it tests encryption as well.

--- DieFledermaus.Tests/Program.cs
+++ DieFledermaus.Tests/Program.cs
@@ -6,20 +6,34 @@ namespace DieFledermaus.Tests
 {
     class Program
     {
+        const int bigBufferLength = 70000;
         [STAThread]
         static void Main(string[] args)
         {
-            const int bigBufferLength = 70000;
             byte[] bigBuffer = new byte[bigBufferLength];
             for (int i = 0; i < bigBufferLength; i++)
                 bigBuffer[i] = (byte)(i + 1);
 
+            byte[] key = new byte[32];
+            Random rng = new Random(32);
+            rng.NextBytes(key);
+
+            GetMode(bigBuffer, key, MausEncryptionFormat.None);
+            Console.WriteLine();
+            GetMode(bigBuffer, key, MausEncryptionFormat.Aes);
+        }
+
+        private static void GetMode(byte[] bigBuffer, byte[] key, MausEncryptionFormat mode)
+        {
+            Console.WriteLine("Encryption: " + mode);
             Console.WriteLine("Initializing MemoryStream.");
             using (MemoryStream ms = new MemoryStream())
             {
-                using (DieFledermausStream ds = new DieFledermausStream(ms, CompressionMode.Compress, true))
+                using (DieFledermausStream ds = new DieFledermausStream(ms, mode, true))
                 using (BinaryWriter writer = new BinaryWriter(ds))
                 {
+                    if (mode != MausEncryptionFormat.None)
+                        ds.Key = key;
                     Console.WriteLine("Length before writing 9: " + ms.Length);
                     writer.Write(9);
                     Console.WriteLine("Length after writing 9: " + ms.Length);
@@ -28,9 +42,13 @@ namespace DieFledermaus.Tests
                 }
                 Console.WriteLine("Length after DieFledermausStream is closed: " + ms.Length);
                 ms.Seek(0, SeekOrigin.Begin);
+                Console.WriteLine("Position before constructor: 0");
                 using (DieFledermausStream ds = new DieFledermausStream(ms, CompressionMode.Decompress, true))
                 using (BinaryReader reader = new BinaryReader(ds))
                 {
+                    if (mode != MausEncryptionFormat.None)
+                        ds.Key = key;
+
                     Console.WriteLine("Position before read: " + ms.Position);
                     Console.WriteLine(reader.ReadInt32());
                     Console.WriteLine("Position during read: " + ms.Position);
