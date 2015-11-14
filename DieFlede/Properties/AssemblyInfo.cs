1c851c0ec6ddff823901273948bff5c4b983264f
KimikoMuffin <kimiko.muffin@gmail.com>
2015-11-13 22:05:29 -0500

    Changed DieFlede to use .Net 4.5, because .Net 4.5 works in Mono.

--- DieFlede/Properties/AssemblyInfo.cs
+++ DieFlede/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DieFled command-line utility.")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DieFled")]
+[assembly: AssemblyDescription("DieFledermaus command-line utility.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DieFled")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
