43e8177cce3df19b8705e8a75a4240737cf9e087
KimikoMuffin <kimiko.muffin@gmail.com>
2015-11-22 12:25:05 -0500

    Oops!

---
+++ DieFledermaus/DieFledermauZArchiveEntry.cs
@@ -0,0 +1,98 @@
+﻿#region BSD license
+/*
+Copyright © 2015, KimikoMuffin.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+3. The names of its contributors may not be used to endorse or promote 
+   products derived from this software without specific prior written 
+   permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using System;
+using System.IO;
+using System.Security;
+
+using DieFledermaus.Globalization;
+
+namespace DieFledermaus
+{
+    /// <summary>
+    /// Represents a single file entry in a <see cref="DieFledermauZArchive"/>.
+    /// </summary>
+    public class DieFledermauZArchiveEntry : DieFledermauZItem
+    {
+        internal DieFledermauZArchiveEntry(DieFledermauZArchive archive, string path, ICompressionFormat compFormat, MausEncryptionFormat encFormat)
+            : base(archive, path, compFormat, encFormat)
+        {
+        }
+
+        private MausBufferStream _writingStream;
+
+
+        /// <summary>
+        /// Opens the archive entry for writing.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ObjectDisposedException">
+        /// The current instance has been deleted.
+        /// </exception>
+        /// <exception cref="NotSupportedException">
+        /// <para><see cref="DieFledermauZItem.Archive"/> is in read-only mode.</para>
+        /// <para>-OR-</para>
+        /// <para>The current instance has already been open for writing.</para>
+        /// </exception>
+        public Stream OpenWrite()
+        {
+            lock (_lock)
+            {
+                EnsureCanWrite();
+                if (_writingStream != null)
+                    throw new InvalidOperationException(TextResources.ArchiveAlreadyWritten);
+
+                _writingStream = new MausBufferStream();
+                _writingStream.Disposing += _writingStream_Disposing;
+                return _writingStream;
+            }
+        }
+
+        private void _writingStream_Disposing(object sender, EventArgs e)
+        {
+            _writingStream.Reset();
+            _writingStream.BufferCopyTo(MausStream);
+        }
+
+        internal override MausBufferStream GetWritten()
+        {
+            if (_writingStream == null || _writingStream.CanRead)
+                throw new InvalidOperationException(string.Format(TextResources.ArchiveNotWritten, MausStream.Filename));
+            return base.GetWritten();
+        }
+
+        internal override void DoDelete()
+        {
+            if (_writingStream != null)
+                _writingStream.Disposing -= _writingStream_Disposing;
+            base.DoDelete();
+        }
+    }
+}
