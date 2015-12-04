33db36ac7bbecba45a9d50ffb38776d68e809433
KimikoMuffin <kimiko.muffin@gmail.com>
2015-12-03 19:34:10 -0500

    Added "Comment" to archive entry.

--- DieFledermaus/DieFledermauZArchiveEntry.cs
+++ DieFledermaus/DieFledermauZArchiveEntry.cs
@@ -101,6 +101,28 @@ namespace DieFledermaus
             }
         }
 
+        /// <summary>
+        /// Gets and sets a comment on the file.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">
+        /// In a set operation, the current instance has been deleted.
+        /// </exception>
+        /// <exception cref="NotSupportedException">
+        /// In a set operation, the current instance is in read-only mode.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// In a set operation, the specified value is not <c>null</c>, and has a length which is equal to 0 or which is greater than 65536 UTF-8 bytes.
+        /// </exception>
+        public string Comment
+        {
+            get { return MausStream.Comment; }
+            set
+            {
+                EnsureCanWrite();
+                MausStream.Comment = value;
+            }
+        }
+
         private MausBufferStream _writingStream;
 
         /// <summary>
