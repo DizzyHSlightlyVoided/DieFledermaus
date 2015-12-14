5185286a1a5e1d77cd87c1dd1b7584536ba7d8b9
KimikoMuffin <kimiko.muffin@gmail.com>
2015-12-14 11:59:18 -0500

    Set a constant RSA key in Tests, so that's one less bit variability to worry about.

--- DieFledermaus.Tests/Program.cs
+++ DieFledermaus.Tests/Program.cs
@@ -22,6 +22,18 @@ namespace DieFledermaus.Tests
 
                 using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
                 {
+                    const string xmlKey = "<RSAKeyValue><!-- Not suitable for use outside the test-app. --><Modulus>2/xGqjup0HUXsCipNDvXX4Y" +
+                        "L0cyc5CK5a5ksyPk6AGFHz4nGneGA72WBBodtziI+cBWujChTQyMMNiQ+h0JOvFYnvMB8u4bkOPrf2rqscA/04nDodkXRqXoIlyHGp0VOBGfA5f4UU" +
+                        "AMZzLwUrLkBpdUwv3e3mQ2jDAz5+eva2cs=</Modulus><Exponent>AQAB</Exponent><P>6WoHJlCCQp8kQGqN+/b73E6nS/x/yYX1kW/xlqDPp" +
+                        "MtPcbUlq5YNqL3+qQT9ynoFHjbnFYfTiupKmillojmKxQ==</P><Q>8UWZ8AvsKkV9QAYxYr6lAtuyEn0Vvqo4xX9rglNKpYIrdhPcr3DnMb8ekIGm" +
+                        "z4KvIy74bbhIUJLeaZa3OQFbTw==</Q><DP>ZAibzdDdMp4vlCfWd/DO2gkfa9JoFb8CknUObca3luHHR20iGtpxOitLE7be6cLHpL5U5QZUJAnrNQ" +
+                        "ye0RqmHQ==</DP><DQ>dgJcG+xI9BgO/hzJVQn4feBlRePGmf56TCdZt2Hz9eYoSdXHMEyh2FQpp/ayV3cNIMFdo5TqUfa0MKMWNRyzww==</DQ><I" +
+                        "nverseQ>23Cnyug4M7ws64AFAcQ1H3sgEWEhCaYH3YQ2Aesv84Cf5cDZMtmVCq0paPx7uDGgl4eZX1jGXyKDql/JSXalbw==</InverseQ><D>Pisw" +
+                        "aUGNPx0oQZ9sGhfjSNqgEn1pxUtO7WqPboiIbL0RR0SffdTR1FXyPb8eOAgTbyeheXiX9zw7Yj2h8iW6DBeCnQbaNA/p3Iw8RBYcTPdz9YLhFOBid2" +
+                        "M26OC/aBozT6h7jLhyuNVkWnFh5Een5QGobtwuvJHkhSIBWUkuevk=</D></RSAKeyValue>";
+
+                    rsa.FromXmlString(xmlKey);
+
                     publicKey = rsa.ExportParameters(false);
                     privateKey = rsa.ExportParameters(true);
                 }
