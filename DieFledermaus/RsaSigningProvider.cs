e0da95f1096e9252b876cb6ad49014a366868b9b
KimikoMuffin <kimiko.muffin@gmail.com>
2015-12-15 11:22:14 -0500

    Went around doing "Remove And Sort Usings", added copyright message.

--- DieFledermaus/RsaSigningProvider.cs
+++ DieFledermaus/RsaSigningProvider.cs
@@ -1,22 +1,39 @@
-﻿using System;
-using System.Collections;
-using System.IO;
-using System.Text;
+﻿#region BSD license
+/*
+Copyright © 2015, KimikoMuffin.
+All rights reserved.
 
-using Org.BouncyCastle.Crypto;
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+3. The names of its contributors may not be used to endorse or promote 
+   products derived from this software without specific prior written 
+   permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using System;
 using Org.BouncyCastle.Asn1;
-using Org.BouncyCastle.Asn1.Nist;
-using Org.BouncyCastle.Asn1.Pkcs;
-using Org.BouncyCastle.Asn1.TeleTrust;
-using Org.BouncyCastle.Asn1.Utilities;
 using Org.BouncyCastle.Asn1.X509;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Engines;
-using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Crypto.Paddings;
-using Org.BouncyCastle.Security;
-using Org.BouncyCastle.Utilities;
 
 namespace DieFledermaus
 {
@@ -47,7 +64,7 @@ namespace DieFledermaus
             if (key.IsPrivate != forSigning)
             {
                 if (key.IsPrivate)
-                    throw new ArgumentException("Private key needed.", nameof(parameters));
+                    throw new ArgumentException("Private key needed for signing.", nameof(parameters));
                 throw new ArgumentException("Public key needed for verification.", nameof(parameters));
             }
             _forSigning = forSigning;
