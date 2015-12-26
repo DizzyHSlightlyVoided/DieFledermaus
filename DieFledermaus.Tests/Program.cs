d86ba09a581681e6ba1df50366468e88cf4a3c30
KimikoMuffin <kimiko.muffin@gmail.com>
2015-12-26 15:50:24 -0500

    Generated 2048-bit RSA keys for the test projects, so there's room to mess around with 512-bit hashes.

--- DieFledermaus.Tests/Program.cs
+++ DieFledermaus.Tests/Program.cs
@@ -24,31 +24,11 @@ namespace DieFledermaus.Tests
                 RsaKeyParameters publicKeySig, privateKeySig, publicKeyEnc, privateKeyEnc;
 
                 {
-                    const string xmlSig = "<RSAKeyValue><!-- Not suitable for use outside the test-app. --><Modulus>2/xGqjup0HUXsCipNDvXX4Y" +
-                        "L0cyc5CK5a5ksyPk6AGFHz4nGneGA72WBBodtziI+cBWujChTQyMMNiQ+h0JOvFYnvMB8u4bkOPrf2rqscA/04nDodkXRqXoIlyHGp0VOBGfA5f4UU" +
-                        "AMZzLwUrLkBpdUwv3e3mQ2jDAz5+eva2cs=</Modulus><Exponent>AQAB</Exponent><P>6WoHJlCCQp8kQGqN+/b73E6nS/x/yYX1kW/xlqDPp" +
-                        "MtPcbUlq5YNqL3+qQT9ynoFHjbnFYfTiupKmillojmKxQ==</P><Q>8UWZ8AvsKkV9QAYxYr6lAtuyEn0Vvqo4xX9rglNKpYIrdhPcr3DnMb8ekIGm" +
-                        "z4KvIy74bbhIUJLeaZa3OQFbTw==</Q><DP>ZAibzdDdMp4vlCfWd/DO2gkfa9JoFb8CknUObca3luHHR20iGtpxOitLE7be6cLHpL5U5QZUJAnrNQ" +
-                        "ye0RqmHQ==</DP><DQ>dgJcG+xI9BgO/hzJVQn4feBlRePGmf56TCdZt2Hz9eYoSdXHMEyh2FQpp/ayV3cNIMFdo5TqUfa0MKMWNRyzww==</DQ><I" +
-                        "nverseQ>23Cnyug4M7ws64AFAcQ1H3sgEWEhCaYH3YQ2Aesv84Cf5cDZMtmVCq0paPx7uDGgl4eZX1jGXyKDql/JSXalbw==</InverseQ><D>Pisw" +
-                        "aUGNPx0oQZ9sGhfjSNqgEn1pxUtO7WqPboiIbL0RR0SffdTR1FXyPb8eOAgTbyeheXiX9zw7Yj2h8iW6DBeCnQbaNA/p3Iw8RBYcTPdz9YLhFOBid2" +
-                        "M26OC/aBozT6h7jLhyuNVkWnFh5Een5QGobtwuvJHkhSIBWUkuevk=</D></RSAKeyValue>";
-
-                    var keyPair = GetKeyPair(xmlSig);
+                    var keyPair = GetKeyPair(Keys.Signature);
                     publicKeySig = (RsaKeyParameters)keyPair.Public;
                     privateKeySig = (RsaKeyParameters)keyPair.Private;
 
-                    const string xmlEnc = "<RSAKeyValue><!-- Not suitable for use outside the test-app. --><Modulus>tLxR5IiTcQ93LArqHYeC4o5" +
-                        "JTBavu6z1G1F9LG2/q/cqZql6bNHzK6XGVYfEVxgl2Lxes3g+4xFcUvC0qPifkyEN8F4l8gmIdtR4KAT+8aSH7xdgVIXexIWDXlk7DfAKHo3KVTy7j" +
-                        "7ZF1596TsGATGrNx3UWH/x/9bR5ltuLfbU=</Modulus><Exponent>AQAB</Exponent><P>zdUnz0+Nh4T6rjTXfoyxCBjCOjoajgfZLO0iw20dD" +
-                        "PlQpUXq5Wp+LSDwjSZCyIJPXkdSLtT19lmxq0xNOgvYwQ==</P><Q>4Mk/DjUo0NEwoHUmJdVG/2qdfWQewI7v8gJKo/GfaVEAPp7urkye4eTuIJz/" +
-                        "ckNef296Emi9cI+ZFmVgY8NN9Q==</Q><DP>dAb3JO6UOlNkt/TDkOugE49ZVVdRhsS30JJwKTeFy71yj2fFTMNmEuxhjT+HH94M/Xk4w3t6lv7in0" +
-                        "wosFLjQQ==</DP><DQ>bgZTFNM0TTF3SbLNn0sLW02GFLAC1WGhVKWGf0RvMI9zPTNxxGLAifUSEWiHKBiNknawG36k6wl+dxXb3jjkWQ==</DQ><I" +
-                        "nverseQ>k/TWXiewe45/3BmL1BPr6uLBPfQ4sYGFcOwXRhKiLDaE6d3VcVXoIb58DH5BHSMtLMkwOBo8mfaCFiKsV0V0xA==</InverseQ><D>Kg1B" +
-                        "HqPabm1zRHebplg/z1fc3QvQQqII+5y3u60jci8VmgJn3kbxReAR6BepSrxvHeEiRa6+LxX8fb3Mwx3p/qz6H6uimCFv+tt8ryaAj6GFjJhysONpZ6" +
-                        "ij1mVn5jsmMgOMRfv68XRRsaoH9a2pYTeht7wF6KnbfxXzBqVbG4E=</D></RSAKeyValue>";
-
-                    keyPair = GetKeyPair(xmlEnc);
+                    keyPair = GetKeyPair(Keys.Encryption);
 
                     publicKeyEnc = (RsaKeyParameters)keyPair.Public;
                     privateKeyEnc = (RsaKeyParameters)keyPair.Private;
