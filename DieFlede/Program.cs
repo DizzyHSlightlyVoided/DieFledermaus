779cab52c2617c62a5dfe6d9e1f38e00e8cd0e95
KimikoMuffin <kimiko.muffin@gmail.com>
2016-05-19 22:42:17 -0400

    More rearranging DieFlede's structure.

--- DieFlede/Program.cs
+++ DieFlede/Program.cs
@@ -38,7 +38,6 @@ using System.Text.RegularExpressions;
 
 using DieFledermaus.Cli.Globalization;
 
-using Org.BouncyCastle.Bcpg.OpenPgp;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
@@ -266,7 +265,7 @@ namespace DieFledermaus.Cli
                 }
                 else index = null;
 
-                keyObj = LoadKeyFile(sigKey.Value, index, create.IsSet, interactive, false);
+                keyObj = PublicKeyReadFuncs.LoadKeyFile(sigKey.Value, index, create.IsSet, new PasswordLoader(interactive), false);
 
                 if (keyObj == null)
                     return Return(-7, interactive);
@@ -297,7 +296,7 @@ namespace DieFledermaus.Cli
                 }
                 else index = null;
 
-                object kObj = LoadKeyFile(encKey.Value, index, !create.IsSet, interactive, true);
+                object kObj = PublicKeyReadFuncs.LoadKeyFile(encKey.Value, index, !create.IsSet, new PasswordLoader(interactive), true);
                 if (kObj == null)
                     return Return(-8, interactive);
 
@@ -1208,7 +1207,7 @@ namespace DieFledermaus.Cli
         }
 
 #if DEBUG
-        private static void GoThrow(Exception e)
+        internal static void GoThrow(Exception e)
         {
             Console.Error.WriteLine(e.GetType().ToString() + ":");
             Console.Error.WriteLine(e.ToString());
@@ -1266,7 +1265,7 @@ namespace DieFledermaus.Cli
             {
                 try
                 {
-                    ss = new string(new ClPassword(interactive).GetPassword());
+                    ss = new string(new PasswordLoader(interactive).GetPassword());
                 }
                 catch (PasswordCancelledException)
                 {
@@ -1413,374 +1412,36 @@ namespace DieFledermaus.Cli
             while (notFound);
             return false;
         }
-
-        internal const int BufferSize = 8192;
-
-        private static AsymmetricKeyParameter LoadKeyFile(string path, BigInteger index, bool getPrivate, ClParamFlag interactive, bool encryption)
-        {
-            if (!File.Exists(path))
-            {
-                Console.Error.WriteLine(TextResources.FileNotFound, path);
-                return null;
-            }
-
-            try
-            {
-                using (FileStream ms = File.OpenRead(path))
-                {
-                    object keyObj = null;
-                    #region PuTTY .ppk
-                    if (keyObj == null)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-                        PuttyPpkReader reader = new PuttyPpkReader(ms, new ClPassword(interactive));
-
-                        try
-                        {
-                            if (reader.Init())
-                            {
-                                if (getPrivate)
-                                {
-                                    if (reader.EncType != "none")
-                                    {
-                                        Console.WriteLine(encryption ? TextResources.KeyFileEnc : TextResources.KeyFileSig, path);
-                                        if (!string.IsNullOrWhiteSpace(reader.Comment))
-                                            Console.WriteLine(reader.Comment);
-                                    }
-
-                                    do
-                                    {
-                                        try
-                                        {
-                                            keyObj = reader.ReadKeyPair().Private;
-                                        }
-                                        catch (InvalidCipherTextException)
-                                        {
-                                            Console.WriteLine(TextResources.BadPassword);
-                                        }
-                                    }
-                                    while (keyObj == null);
-                                }
-                                else keyObj = reader.ReadPublicKey();
-
-                                if (index != null && index.CompareTo(BigInteger.Zero) != 0)
-                                {
-                                    Console.Error.WriteLine(TextResources.IndexFile, index);
-                                    return null;
-                                }
-                            }
-                        }
-                        catch (InvalidDataException)
-                        {
-                            Console.Error.WriteLine(getPrivate ? TextResources.SignBadPrivate : TextResources.SignBadPublic);
-                        }
-                    }
-                    #endregion
-
-                    #region PEM
-                    bool seen = false;
-                    while (keyObj == null)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-                        using (StreamReader sr = new StreamReader(ms, Encoding.UTF8, true, BufferSize, true))
-                        {
-                            PemReader reader = new PemReader(sr, new ClPassword(interactive, seen ? null : path, encryption));
-                            try
-                            {
-                                keyObj = reader.ReadObject();
-
-                                if (keyObj == null) break;
-                                else if (index != null && index.CompareTo(BigInteger.Zero) != 0)
-                                {
-                                    Console.Error.WriteLine(TextResources.IndexFile, index);
-                                    return null;
-                                }
-                            }
-                            catch (InvalidCipherTextException)
-                            {
-                                seen = true;
-                                Console.Error.WriteLine(TextResources.BadPassword);
-                                continue;
-                            }
-                            catch (IOException x)
-                            {
-                                if (x.Message == "base64 data appears to be truncated")
-                                {
-                                    keyObj = null;
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.Error.WriteLine(x.Message);
-#if DEBUG
-                                    GoThrow(x);
-#endif
-                                    return null;
-                                }
-                            }
-                        }
-                    }
-                    #endregion
-
-                    #region PGP - Private Key
-                    if (keyObj == null)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-                        if (index == null) index = BigInteger.Zero;
-
-                        try
-                        {
-                            var decoderStream = PgpUtilities.GetDecoderStream(ms);
-                            PgpSecretKeyRing bundle = new PgpSecretKeyRing(decoderStream);
-
-                            BigInteger counter = BigInteger.ValueOf(-1);
-
-                            foreach (PgpSecretKey pKey in bundle.GetSecretKeys())
-                            {
-                                counter = counter.Add(BigInteger.One);
-
-                                if (counter.CompareTo(index) < 0)
-                                    continue;
-
-                                if (!getPrivate)
-                                {
-                                    keyObj = pKey.PublicKey.GetKey();
-                                    break;
-                                }
-
-                                if (pKey.KeyEncryptionAlgorithm == 0)
-                                {
-                                    PgpPrivateKey privKey = pKey.ExtractPrivateKeyUtf8(null);
-                                    keyObj = privKey.Key;
-                                }
-
-                                seen = false;
-                                do
-                                {
-                                    char[] password = null;
-                                    try
-                                    {
-                                        password = new ClPassword(interactive, seen ? null : path, encryption).GetPassword();
-                                        PgpPrivateKey privKey = pKey.ExtractPrivateKeyUtf8(password);
-
-                                        keyObj = privKey.Key;
-                                    }
-                                    catch (PgpException)
-                                    {
-                                        seen = true;
-                                        if (password != null && password.Length == 0)
-                                            return null;
-                                        Console.Error.WriteLine(TextResources.BadPassword);
-                                    }
-                                }
-                                while (keyObj == null); break;
-                            }
-
-                            if (keyObj == null && counter.CompareTo(index) < 0)
-                            {
-                                Console.Error.WriteLine(TextResources.IndexFile, index);
-                                return null;
-                            }
-                        }
-                        catch (IOException x)
-                        {
-                            if (!x.Message.StartsWith("secret key ring doesn't start with", StringComparison.OrdinalIgnoreCase))
-#if DEBUG
-                                GoThrow(x);
-#else
-                                throw;
-#endif
-                        }
-                    }
-                    #endregion
-
-                    #region PGP - Public Key
-                    if (keyObj == null)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-                        try
-                        {
-                            var decoderStream = PgpUtilities.GetDecoderStream(ms);
-                            PgpPublicKeyRing bundle = new PgpPublicKeyRing(decoderStream);
-
-                            BigInteger counter = BigInteger.ValueOf(-1);
-
-                            foreach (PgpPublicKey pKey in bundle.GetPublicKeys())
-                            {
-                                counter = counter.Add(BigInteger.One);
-
-                                if (counter.CompareTo(index) < 0)
-                                    continue;
-
-                                if (getPrivate)
-                                {
-                                    Console.Error.WriteLine(TextResources.SignNeedPrivate);
-                                    return null;
-                                }
-
-                                keyObj = pKey.GetKey();
-                                break;
-                            }
-
-                            if (keyObj == null && counter.CompareTo(index) < 0)
-                            {
-                                Console.Error.WriteLine(TextResources.IndexFile, index);
-                                return null;
-                            }
-                        }
-                        catch (IOException x)
-                        {
-                            if (!x.Message.StartsWith("public key ring doesn't start with", StringComparison.OrdinalIgnoreCase))
-#if DEBUG
-                                GoThrow(x);
-#else
-                                throw;
-#endif
-                        }
-                    }
-                    #endregion
-
-                    #region OpenSSL .pub/authorized_keys
-                    if (keyObj == null)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-                        if (index == null) index = BigInteger.Zero;
-
-                        using (StreamReader sr = new StreamReader(ms, Encoding.UTF8, true, BufferSize, true))
-                        {
-                            AsymmetricKeyParameter pubKey;
-                            if (PublicKeyReadFuncs.TryGetPublicKey(sr, index, out pubKey))
-                            {
-                                if (getPrivate)
-                                {
-                                    Console.Error.WriteLine(TextResources.SignNeedPrivate);
-                                    return null;
-                                }
-
-                                return pubKey;
-                            }
-                        }
-                    }
-                    #endregion
-
-                    AsymmetricCipherKeyPair pair = keyObj as AsymmetricCipherKeyPair;
-                    if (pair != null)
-                    {
-                        if (getPrivate)
-                        {
-                            if (pair.Private is RsaKeyParameters || pair.Private is DsaKeyParameters || pair.Private is ECKeyParameters)
-                                return pair.Private;
-
-                            Console.Error.WriteLine(TextResources.UnknownKeyType);
-                            return null;
-                        }
-
-                        if (pair.Public is RsaKeyParameters || pair.Public is DsaKeyParameters || pair.Public is ECKeyParameters)
-                            return pair.Public;
-
-                        Console.Error.WriteLine(TextResources.UnknownKeyType);
-                        return null;
-                    }
-
-                    if (!(keyObj is AsymmetricKeyParameter))
-                    {
-                        Console.Error.WriteLine(TextResources.UnknownKeyType);
-                        return null;
-                    }
-
-                    RsaKeyParameters singleRsa = keyObj as RsaKeyParameters;
-                    if (singleRsa != null)
-                    {
-                        if (singleRsa is RsaPrivateCrtKeyParameters)
-                            return singleRsa;
-
-                        if (getPrivate)
-                        {
-                            Console.Error.WriteLine(singleRsa.IsPrivate ? TextResources.SignBadPrivate : TextResources.SignNeedPrivate);
-                            return null;
-                        }
-
-                        if (singleRsa.IsPrivate)
-                        {
-                            Console.Error.WriteLine(TextResources.SignBadPublic);
-                            return null;
-                        }
-
-                        return singleRsa;
-                    }
-
-                    DsaKeyParameters singleDsa = keyObj as DsaKeyParameters;
-                    if (singleDsa != null)
-                    {
-                        if (singleDsa is DsaPrivateKeyParameters)
-                            return singleDsa;
-
-                        if (getPrivate)
-                        {
-                            Console.Error.WriteLine(singleDsa.IsPrivate ? TextResources.SignBadPrivate : TextResources.SignNeedPrivate);
-                            return null;
-                        }
-
-                        if (singleDsa is DsaPublicKeyParameters)
-                            return singleDsa;
-                        Console.Error.WriteLine(TextResources.SignBadPublic);
-                        return null;
-                    }
-
-                    ECKeyParameters singleEcdsa = keyObj as ECKeyParameters;
-                    if (singleEcdsa == null)
-                    {
-                        Console.Error.WriteLine(TextResources.UnknownKeyType);
-                        return null;
-                    }
-
-                    if (singleEcdsa is ECPrivateKeyParameters)
-                        return singleEcdsa;
-
-                    if (getPrivate)
-                    {
-                        Console.Error.WriteLine(singleEcdsa.IsPrivate ? TextResources.SignBadPrivate : TextResources.SignNeedPrivate);
-                        return null;
-                    }
-
-                    if (singleEcdsa is ECPublicKeyParameters)
-                        return singleEcdsa;
-
-                    Console.Error.WriteLine(TextResources.SignBadPublic);
-                    return null;
-                }
-            }
-            catch (PasswordCancelledException)
-            {
-                return null;
-            }
-            catch (Exception x)
-            {
-                Console.Error.WriteLine(x.Message);
-#if DEBUG
-                GoThrow(x);
-#endif
-                return null;
-            }
-        }
     }
 
-    internal class ClPassword : IPasswordFinder
+    internal class PasswordLoader : IPasswordFinder
     {
         private ClParam _interactive;
-        private string _path;
-        private bool _enc;
 
-        public ClPassword(ClParamFlag interactive, string path, bool encryption)
+        public PasswordLoader(ClParamFlag interactive, string path, bool encryption)
         {
             _interactive = interactive;
             _path = path;
             _enc = encryption;
         }
 
-        public ClPassword(ClParamFlag interactive)
+        public PasswordLoader(ClParamFlag interactive)
             : this(interactive, null, true)
         {
+        }
+
+        private string _path;
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+
+        private bool _enc;
+        public bool Encryption
+        {
+            get { return _enc; }
+            set { _enc = value; }
         }
 
         public char[] GetPassword()
